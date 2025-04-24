06367c156df434f7c840c1194c543b3908dd2131
rasunaaa <2311104043@ittelkom-pwt.ac.id>
2025-04-24 23:44:03 +0700

    commit all tugas

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TpModul6/TpModul6/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Muhammad Shafiq Rasuna");
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+    }
+}
---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TpModul6/TpModul6/SayaTubeVidio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+    }
+}
